f6f4c4047290564c4ffb62f97f44f217bf4abb31
Yan <ygiber@seatgeek.com>
2023-09-04 17:03:15 +0300

    add basic code for the project

---
+++ tests/sanity.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').press('CapsLock');
+  await page.locator('[data-test="username"]').fill('');
+  await page.locator('[data-test="username"]').press('CapsLock');
+  await page.locator('[data-test="username"]').fill('standart');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="username"]').press('Tab');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+  await page.locator('a').filter({ hasText: '2' }).click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('[data-test="cancel"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('[data-test="firstName"]').click();
+  await page.locator('[data-test="firstName"]').fill('Yan');
+  await page.locator('[data-test="firstName"]').press('Tab');
+  await page.locator('[data-test="lastName"]').fill('ba');
+  await page.locator('[data-test="lastName"]').press('Tab');
+  await page.locator('[data-test="postalCode"]').fill('12345');
+  await page.locator('[data-test="continue"]').click();
+  await page.locator('[data-test="finish"]').click();
+  await page.locator('[data-test="back-to-products"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.getByRole('link', { name: 'Reset App State' }).click();
+  await page.getByRole('link', { name: 'Logout' }).click();
+});
